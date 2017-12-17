9f2f00d19d8d648c745d7900e22e362f973e14f5
Uthaeus <Uth@github.com>
2017-12-16 17:39:45 -0700

    Implemented authorization for blogs

---
+++ app/models/guest_user.rb
@@ -0,0 +1,13 @@
+class GuestUser < User
+  attr_accessor :name, :first_name, :last_name, :email
+
+  def guest_user
+    guest = GuestUser.new
+    guest.name = "Guest User"
+    guest.first_name = "Guest"
+    guest.last_name = "User"
+    guest.email = "guest@example.com"
+    guest
+  end
+  
+end
